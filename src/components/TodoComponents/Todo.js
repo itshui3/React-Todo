503a9128415dbaf7f1f10e70769f63815a547bb0
Hui <jeffrey.x.hui@gmail.com>
2019-12-02 12:13:27 -0800

    added strikethrough functionaity, this should be MVP. will double check next commit

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,25 +1,57 @@
 import React from 'react';
+import './Todo.scss';
 
 export default class Todo extends React.Component {
   constructor() {
     super()
-
+    this.state = {
+      complete: false
+    }
   }
 // these props need to be receiving information I can plug in that the
 // user typed into the form that should be saved in state on change and 
 // iterated in on submit
+  handleCompletion = ev => {
+    this.setState({ complete: !this.state.complete })
+  }
+
+  // setStrikethrough = complete = {
+  //   if (complete) {
+  //     const strikethrough = { text-decoration: 'line-through' }
+  //     return strikethrough
+  //   } else {
+  //     return {}
+  //   }
+
+  // }
 
   render() {
+  
+
     return(
       <div>
         <ul>
-          <li>{this.props.note.task}</li>
-          <li>{this.props.note.time}</li>
-          <li>
-            <button onClick={() => this.props.delete(this.props.index)}>Delete</button>
-            <button onClick={() => this.props.edit(this.props.index)}>Edit</button>
-          </li>
+          <li 
+            className={`${
+              this.state.complete ?
+              'complete' :
+              'incomplete'
+            }`}
+            onClick={this.handleCompletion}
+          >{this.props.note.task}</li>
+          <li 
+            className={`${
+              this.state.complete ?
+              'complete' :
+              'incomplete'
+            }`}
+            onClick={this.handleCompletion}
+          >{this.props.note.time}</li>
         </ul>
+        <div>
+          <button onClick={() => this.props.delete(this.props.index)}>Delete</button>
+          <button onClick={() => this.props.edit(this.props.index)}>Edit</button>
+        </div>
 
       </div>
       
